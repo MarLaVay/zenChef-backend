d755fe003a547c69d40098fdd0ebbd08f75a44ef
Farrowlia <79092276+Farrowlia@users.noreply.github.com>
2021-10-18 21:33:41 +0200

    Initial project version

---
+++ src/main/java/com/maryan/zenchef/ZenChefApplication.java
@@ -0,0 +1,13 @@
+package com.maryan.zenchef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenChefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenChefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maryan/zenchef/ZenChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryan.zenchef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenChefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
