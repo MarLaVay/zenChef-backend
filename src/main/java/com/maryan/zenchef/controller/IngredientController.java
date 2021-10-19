fa65c3e12db2a6e96baff79b5235b2b6937108ac
demzem <yann.delemos@gmail.com>
2021-10-19 15:02:22 +0200

    Creation controllers
    Ingredient
    Recipe

---
+++ src/main/java/com/maryan/zenchef/controller/IngredientController.java
@@ -0,0 +1,10 @@
+package com.maryan.zenchef.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ingredient")
+public class IngredientController {
+
+}
---
+++ src/main/java/com/maryan/zenchef/controller/RecipeController.java
@@ -0,0 +1,12 @@
+package com.maryan.zenchef.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recipe")
+public class RecipeController {
+	
+	
+
+}
